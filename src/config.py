9f2a6889541e8066cb8127c49c46955c8aad3bf2
ElizaZh <elizazhang087@gmail.com>
2019-04-22 15:39:11 -0700

    adjust path in config

--- src/config.py
+++ src/config.py
@@ -22,20 +22,25 @@ curr_path = osp.dirname(osp.abspath(__file__))
 model_dir = osp.join(curr_path, '..', 'models')
 if not osp.exists(model_dir):
     print('Fix path to models/')
-    import ipdb
-    ipdb.set_trace()
+video_dir = osp.join(curr_path, '..', 'data')
+output_dir = osp.join(curr_path, '..', 'refine_output')
+
 SMPL_MODEL_PATH = osp.join(model_dir, 'neutral_smpl_with_cocoplus_reg.pkl')
 SMPL_FACE_PATH = osp.join(curr_path, 'tf_smpl', 'smpl_faces.npy')
 
 # Default pred-trained model path for the demo.
-PRETRAINED_MODEL = '/home/kanazawa/projects/hmr_sfv/models/Feb12_2100_save75_model.ckpt-667589'
-# PRETRAINED_MODEL = osp.join(model_dir, 'model.ckpt-667589')
+#PRETRAINED_MODEL = '/home/kanazawa/projects/hmr_sfv/models/Feb12_2100_save75_model.ckpt-667589'
+PRETRAINED_MODEL = osp.join(model_dir, 'model.ckpt-667589')
 
 flags.DEFINE_string('smpl_model_path', SMPL_MODEL_PATH,
                     'path to the neurtral smpl model')
 flags.DEFINE_string('smpl_face_path', SMPL_FACE_PATH,
                     'path to smpl mesh faces (for easy rendering)')
 flags.DEFINE_string('load_path', PRETRAINED_MODEL, 'path to trained model')
+
+flags.DEFINE_string('video_dir', video_dir, 'path to original videos')
+
+flags.DEFINE_string('out_dir', output_dir, 'path to the output')
 
 
 flags.DEFINE_integer('img_size', 224,
