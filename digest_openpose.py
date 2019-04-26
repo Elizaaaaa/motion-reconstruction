7e5d16096c831e7d45d94db0ec4a61fe6ada07a8
ElizaZh <elizazhang087@gmail.com>
2019-04-26 14:29:04 -0700

    3d joints is not available with single camera noooooo

--- digest_openpose.py
+++ digest_openpose.py
@@ -1,5 +1,26 @@
 import deepdish as dd
+import numpy as np
+import pandas as pd
 
+joints_names = ['Ankle.R_x', 'Ankle.R_y', 'Ankle.R_z',
+                   'Knee.R_x', 'Knee.R_y', 'Knee.R_z',
+                   'Hip.R_x', 'Hip.R_y', 'Hip.R_z',
+                   'Hip.L_x', 'Hip.L_y', 'Hip.L_z',
+                   'Knee.L_x', 'Knee.L_y', 'Knee.L_z',
+                   'Ankle.L_x', 'Ankle.L_y', 'Ankle.L_z',
+                   'Wrist.R_x', 'Wrist.R_y', 'Wrist.R_z',
+                   'Elbow.R_x', 'Elbow.R_y', 'Elbow.R_z',
+                   'Shoulder.R_x', 'Shoulder.R_y', 'Shoulder.R_z',
+                   'Shoulder.L_x', 'Shoulder.L_y', 'Shoulder.L_z',
+                   'Elbow.L_x', 'Elbow.L_y', 'Elbow.L_z',
+                   'Wrist.L_x', 'Wrist.L_y', 'Wrist.L_z',
+                   'Neck_x', 'Neck_y', 'Neck_z',
+                   'Head_x', 'Head_y', 'Head_z',
+                   'Nose_x', 'Nose_y', 'Nose_z',
+                   'Eye.L_x', 'Eye.L_y', 'Eye.L_z',
+                   'Eye.R_x', 'Eye.R_y', 'Eye.R_z',
+                   'Ear.L_x', 'Ear.L_y', 'Ear.L_z',
+                   'Ear.R_x', 'Ear.R_y', 'Ear.R_z']
 # This is what we want.
 joint_names = [
     'R Ankle', 'R Knee', 'R Hip', 'L Hip', 'L Knee', 'L Ankle', 'R Wrist',
@@ -16,6 +37,7 @@ op_names = [
     'L Shoulder',
     'L Elbow',
     'L Wrist',
+    'Mid Hip',
     'R Hip',
     'R Knee',
     'R Ankle',
@@ -26,12 +48,29 @@ op_names = [
     'L Eye',
     'R Ear',
     'L Ear',
+    'L BigToe',
+    'L SmallToe',
+    'L Heel',
+    'R BigToe',
+    'R SmallToe',
+    'R Heel',
+    'Background',
     'Head',
 ]
+permute_order = np.array([op_names.index(name) for name in joint_names])
+print(permute_order)
 
+keypoints = []
 
 all_frames = dd.io.load('/Users/eliza/Documents/motion-reconstruction/output/vault_bboxes.h5')
 for frame in all_frames:
     for i, bbox, joints in all_frames[frame]:
-        print('placeholder')
+        print(len(joints))
+        joints = np.vstack((joints, np.zeros((1, 3))))
+        print(len(joints))
+        new_joints = joints[permute_order, :]
+        keypoints.append(new_joints)
 
+for keypoint in keypoints:
+    joints_export = pd.DataFrame(keypoint.reshape(1, 57), columns=joints_names)
+    joints_export.index.name = 'frame'
