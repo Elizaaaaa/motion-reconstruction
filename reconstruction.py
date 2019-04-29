d100b8b00bbe2ef42c36d6557f71ed0ca1feea7f
Elizaaaaa <elizazhang087@gmail.com>
2019-04-29 20:22:13 +0000

    fix the path

--- reconstruction.py
+++ reconstruction.py
@@ -410,12 +410,13 @@ def run_video(frames, per_frame_people, config, output_path):
 extensions_video = {".mov", ".mp4", ".avi"}
 video_dir = './data/'
 output_dir = './output/'
+openpose_dir = '~/workspace/openpose/'
 
 for filename in os.listdir(video_dir):
     for ext in extensions_video:
         if filename.endswith(ext):
             filepath = video_dir+filename
-            cmd_command = '/Users/eliza/Documents/openpose/build/examples/openpose/openpose.bin --video {0} --write_json {1} --write_images {2} --write_images_format jpg --model_folder /Users/eliza/Documents/openpose/models'.format(filepath, output_dir, output_dir)
+            cmd_command = '~/workspace/openpose/build/examples/openpose/openpose.bin --video {0} --write_json {1} --write_images {2} --write_images_format jpg --model_folder ~/workspace/openpose/models --display 0'.format(filepath, output_dir, output_dir)
             print(cmd_command)
 
 run = os.system(cmd_command)
