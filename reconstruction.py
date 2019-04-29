f1df9611904463ba25c0358589930e4f821ea581
Elizaaaaa <elizazhang087@gmail.com>
2019-04-29 21:02:49 +0000

    deal with the path

--- reconstruction.py
+++ reconstruction.py
@@ -409,6 +409,11 @@ video_dir = './data/'
 output_dir = './output/'
 openpose_dir = '~/workspace/openpose/'
 
+if not os.path.exists(output_dir+"jsons/"):
+    os.mkdir(output_dir+"jsons/")
+if not os.path.exists(output_dir+"images/"):
+    os.mkdir(output_dir+"images/")
+
 for filename in os.listdir(video_dir):
     for ext in extensions_video:
         if filename.endswith(ext):
@@ -419,12 +424,12 @@ for filename in os.listdir(video_dir):
             output_image_dir = output_dir + "images/" + movement + "/"
             if os.path.exists(output_json_dir):
                 print("jason output alread exists!")
-                os.remove(output_json_dir)
+                os.system('rm -rf {}'.format(output_json_dir+"/*"))
             else:
                 os.mkdir(output_json_dir)
             if os.path.exists(output_image_dir):
                 print("image output already exists!")
-                os.remove(output_image_dir)
+                os.system('rm -rf {}'.format(output_image_dir+"/*"))
             else:
                 os.mkdir(output_image_dir)
 
