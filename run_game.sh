cde33a70eedcd89a18e202f6f4910b29aa2725af
Elizaaaaa <elizazhang087@gmail.com>
2019-05-03 00:06:29 +0000

    resize the video before processing

--- run_game.sh
+++ run_game.sh
@@ -1,16 +1,20 @@
 rm ./data/*
+rm ./original_videos/*
+
 #gsutil cp gs://mimicus-videos/user-uploads/* ./data/
-gdrive download --recursive 15uXxRZAiomTdDPp1T933nETZM_oBB7kC
-sudo mv demo/videos/* ./data/
-rm -rf demo
+#gdrive download --recursive 15uXxRZAiomTdDPp1T933nETZM_oBB7kC
+gsutil cp gs://mimicus-videos/debug/45degree/* ./original_videos/
+#sudo mv demo/videos/* ./original_videos/
+#rm -rf demo
 echo "Downloads finished."
 
 echo "Resize the input videos"
-for f in output/*; do
+for f in original_videos/*; do
     video_name=$(basename "$f")
     no_ext="${video_name%.*}"
-    output_name="${no_ext}.mp4"
-    ffmpeg -i $video_name -filter:v scale=720:-1 -c:a copy $output_name
+    output_name="data/${no_ext}.mp4"
+    echo "resize the $video_name and exprot to $output_name"
+    ffmpeg -i $f -filter:v scale=720:-1 -c:a copy $output_name
 done
 
 rm -rf ./output/*
